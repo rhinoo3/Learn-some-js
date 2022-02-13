a764d546f5754f0361ffe20f87ad79ef6d97ee73
rhinoo3 <ryan.nichollaporte@gmail.com>
2022-02-12 19:31:10 -0500

    adding it to git so i can access on my computer

---
+++ script.js
@@ -0,0 +1,14 @@
+let country = ('Canada');
+let continent = ('North America');
+let population = (38000000);
+
+if (population > 37000000) {
+  population = '38M';
+
+}
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+if (country === "Canada") alert('this is nothingness')
