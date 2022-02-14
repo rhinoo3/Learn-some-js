c573ec861b6e5340e34746f88e3adf6806042964
rhinoo3 <ryan.nichollaporte@gmail.com>
2022-02-14 13:22:07 -0500

    finished up to challenge 1

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-/* value and variables
+//value and variables
+/*
 let country = ('Canada');
 let continent = ('North America');
 let population = (38000000);
@@ -36,7 +37,89 @@ year = 1992;
 console.log(typeof year);
 
 console.log(typeof null);
+
+//<b>let, const, and var</b> <- Old way
+
+//reassigning variable. (mutated variable)
+let age = 30;
+age = 31;
+
+//const cannot be mutated. ex birthyear
+const birthYear = 1992;
+// birthYear = 1990;
+
+// const job; <- error
+
+// const job;
+
+var job = 'programmer';
+job = 'teacher'
+
+lastName = 'Laporte';
+console.log(lastName);
+
+
+//Basic operators
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5; //15
+x += 10; //x = x + 10 = 25
+x *= 4; //x = x * 4 =100
+x++; // x = x + 1 = 101
+x--; // x = x - 1 = 100
+x--; // x = x - 1 = 99
+console.log(x);
+
+// Comparison operators
+//boolean...asking if true or false and will provide answer in console
+// Greater than >, Less than <,Greater equal >=, Less than equal <=
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+//based off the operator precedence. subtraction left to right (higher precedence), equal is right to left (lower precedence) 
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+
+
+//coding Challenge #1
+// let massMark = 78;
+// let heightMark = 1.69;
+// let massJohn = 92;
+// let heightJohn = 1.95;
+
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const markBMI = massMark / heightMark ** 2;
+const johnBMI = massJohn / heightJohn ** 2;
+const markHigherBMI = markBMI > johnBMI;
+
+console.log(johnBMI, markBMI, markHigherBMI);
 */
-
-
-
